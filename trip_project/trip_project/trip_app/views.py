2242802162861765d17d9c36af34ee65544c4d24
Yasaswi Poolla <yasaswi.poolla@itcrats.com>
2020-07-04 18:55:00 +0530

    fixes

--- trip_project/trip_project/trip_app/views.py
+++ trip_project/trip_project/trip_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from trip_project.trip_app.api.serializers import UserProfileSerializer
 
-# Create your views here.
+class UserUpdate(APIView):
+    parser_classes = [MultiPartParser, FormParser]
+    permission_classes = [IsAuthenticated]
+
+
+    def post(self, request, format=None):
+        serializer = UserProfileSerializer(data=request.data, instance=request.user)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
